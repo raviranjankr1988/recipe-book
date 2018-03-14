2299e6c51d8242e39aef78b721cc37490723a6c8
raviranjankr1988 <raviranjankr@gmail.com>
2018-03-14 09:16:06 +0530

    added authentication with firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,17 +4,23 @@ import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
 
   storeRecipes() {
-    return this.http.put('https://recipe-book-1d3b8.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put('https://recipe-book-1d3b8.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://recipe-book-1d3b8.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+    this.http.get('https://recipe-book-1d3b8.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
